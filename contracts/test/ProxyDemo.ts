243b4101a9cbb8a30838a2e0b0ca1290cd4d1b3c
George Bibilashvili <giorgi.bibilashvili89@gmail.com>
2024-12-08 22:05:15 +0400

    fix: tests

--- contracts/test/ProxyDemo.ts
+++ contracts/test/ProxyDemo.ts
@@ -6,51 +6,25 @@ import UpgradeModule from "../ignition/modules/UpgradeModule";
 import { Demo, DemoV2 } from "../typechain-types";
 
 describe("Demo Proxy", function () {
-  // describe("Proxy interaction", async function () {
-  //   it("Should be interactable via proxy", async function () {
-  //     const [, otherAccount] = await ethers.getSigners();
-
-  //     const { demo } = await ignition.deploy(ProxyModule);
-  //     const demoContract = demo as unknown as Demo;
-
-  //     expect(await demoContract.connect(otherAccount).version()).to.equal("1.0.0");
-  //   });
-  // });
-
-  describe("Upgrading", function () {
-    // it("Should have upgraded the proxy to DemoV2", async function () {
-    //   const [, otherAccount] = await ethers.getSigners();
-
-    //   const { demo } = await ignition.deploy(UpgradeModule);
-    //   const demoContract = demo as unknown as DemoV2;
-
-    //   expect(await demoContract.connect(otherAccount).version()).to.equal("2.0.0");
-    // });
-
-    // it("Should have set the name during upgrade", async function () {
-    //   const [, otherAccount] = await ethers.getSigners();
-
-    //   const { demo } = await ignition.deploy(UpgradeModule);
-    //   const demoContract = demo as unknown as DemoV2;
-
-    //   expect(await demoContract.connect(otherAccount).name()).to.equal("Example Name");
-    // });
-    it("Old value still accesable after upgrade", async function () {
+  describe("Proxy interaction", async function () {
+    it("Should be interactable via proxy", async function () {
       const [, otherAccount] = await ethers.getSigners();
 
-      const { demo: oldDemo } = await ignition.deploy(ProxyModule);
-      const oldDemoContract = oldDemo as unknown as DemoV2;
+      const { demo } = await ignition.deploy(ProxyModule);
+      const demoContract = demo as unknown as Demo;
 
-      await oldDemoContract.setName("Example Name v1");
+      expect(await demoContract.connect(otherAccount).version()).to.equal("1.0.0");
+    });
+  });
 
-      expect(await oldDemoContract.connect(otherAccount).name()).to.equal("Example Name v1");
+  describe("Upgrading", function () {
+    it("Should have upgraded the proxy to DemoV2", async function () {
+      const [, otherAccount] = await ethers.getSigners();
 
-      const { demo: newDemo } = await ignition.deploy(UpgradeModule);
-      const newDemoContract = newDemo as unknown as DemoV2;
+      const { demo } = await ignition.deploy(UpgradeModule);
+      const demoContract = demo as unknown as DemoV2;
 
-      expect(await newDemoContract.connect(otherAccount).name()).to.equal("Example Name v1");
-
-      // expect(await demoContract.connect(otherAccount).name()).to.equal("Example Name");
+      expect(await demoContract.connect(otherAccount).version()).to.equal("2.0.0");
     });
   });
 });
